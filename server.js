1a85569b0863bacc4534accce3f62b963bf224db
Tebyt <chenpan5532323@gmail.com>
2016-04-27 01:18:38 -0400

    test

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ var client = SNSClient(function(err, message) {
     else console.log(message);
 });
  
-app.post('/api/tweets', client);
+// app.post('/api/tweets', client);
 app.set('port', process.env.PORT || 3000);
 app.listen(app.get('port'), function () {
     console.log('Listening on port ' + app.get('port'))
