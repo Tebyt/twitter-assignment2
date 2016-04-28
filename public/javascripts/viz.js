3143d44abc6f31764061acc448fa3600f55491a6
Tebyt <chenpan5532323@gmail.com>
2016-04-28 01:55:21 -0400

    delete debuting

--- public/javascripts/viz.js
+++ public/javascripts/viz.js
@@ -69,9 +69,7 @@ function registerMarker(name, color) {
 function registerSocket() {
     socket = io.connect();
     socket.on('tweet', function (tweet) {
-        console.log(tweet);
         var point = formatPoint(JSON.parse(tweet));
-        console.log(point);
         showPoint(point);
         d3.select("#tweet").text(tweet.text);
     });
