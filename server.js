64ca25922ed2c772b49f59b6d60c8a4d2d0fdd50
Tebyt <chenpan5532323@gmail.com>
2016-04-28 02:03:52 -0400

    change to index.html

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ var io = require('socket.io')(server);
 
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
-var path = require('path');
 // var compression = require('compression');
 
 app.set('port', process.env.PORT || 3000);
@@ -13,14 +12,12 @@ server.listen(app.get('port'), function (err, resp) {
     if (err) console.log(err);
     console.log('Listening on port ' + app.get('port'))
 });
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 // app.use(compression);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 
 
 
@@ -28,22 +25,5 @@ io.on('connection', function (socket) {
     console.log("socket.io connected");
 });
 
-var index = require('./routes/index');
-app.use('/', index);
 var api_tweets = require('./routes/api_tweets')(io);
 app.use('/api/tweets', api_tweets)
-
-
-
-// app.use(function (err, req, res, next) {
-//     res.status(err.status || 500);
-//     res.render('error', {
-//         message: err.message,
-//         error: err
-//     });
-// });
-// Socket.io
-// var io = require('socket.io')(server);
-// io.on('connection', function (socket) {
-//     console.log("connected");
-// });
